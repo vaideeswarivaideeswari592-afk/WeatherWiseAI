b1bf68c938747fd99446fcd585a486d7b5181aa1
vaideeswarivaideeswari592-afk <vaideeswarivaideeswari592@gmail.com>
2026-09-25 20:19:19 +0530

    Delete src directory

--- src/routes/weatherRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getWeather } = require('../controllers/weatherController');
-
-router.get('/:city', getWeather);
-
-module.exports = router;
